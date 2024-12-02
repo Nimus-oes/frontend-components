6fee112888d547a816d6ea0b159f3d9455ae807a
Nimus-oes <sseo.code@gmail.com>
2024-12-02 13:49:37 +0900

    If condition changed

--- 02_menu_collapsible/main.js
+++ 02_menu_collapsible/main.js
@@ -4,10 +4,10 @@ for (let i = 0; i < sectionTitle.length; i++) {
   sectionTitle[i].addEventListener("click", function () {
     this.classList.toggle("active");
     const sectionContent = this.nextElementSibling;
-    if (sectionContent.style.maxHeight) {
-      sectionContent.style.maxHeight = null;
-    } else {
+    if (this.classList.contains("active")) {
       sectionContent.style.maxHeight = sectionContent.scrollHeight + "px";
+    } else {
+      sectionContent.style.maxHeight = null;
     }
   });
 }
